caa0fa38cea02cc3e77ac089550364422209cd4b
ferdinandop <f.primerano90@gmail.com>
2021-12-05 22:01:00 +0100

    feat: add day1-part2 ruby solution

--- src/day1/main.rb
+++ src/day1/main.rb
@@ -1,13 +1,25 @@
 module Main
   class << self
-    def run(input_file)
+    def part1(input_file)
       count = 0
       parse_input(input_file).inject do |prev, current|
         count += 1 if current > prev
         current
       end
 
-      puts count
+      puts "Part 1: #{count}"
+    end
+
+    def part2(input_file)
+      count = 0
+      data = parse_input(input_file)
+      (0..data.length-4).each do |idx|
+        next if data.slice(idx+1, 3).reduce(:+) <= data.slice(idx, 3).reduce(:+)
+
+        count += 1
+      end
+
+      puts "Part 2: #{count}"
     end
 
     private
@@ -18,4 +30,5 @@ module Main
   end
 end
 
-Main.run("src/day1/input.txt")
+Main.part1("src/day1/input.txt")
+Main.part2("src/day1/input.txt")
