d307179cbba25552e0fa8c524d5e5eb19c79842b
ferdinandop <f.primerano90@gmail.com>
2021-12-10 12:35:50 +0100

    feat: add day 10 ruby solution

---
+++ src/day10/main.rb
@@ -0,0 +1,98 @@
+require_relative "../base"
+
+class Main < Base
+  attr_accessor :score
+
+  SCORES = {
+    ")" => 3,
+    "]" => 57,
+    "}" => 1197,
+    ">" => 25137
+  }.freeze
+
+  POINTS = {
+    ")" => 1,
+    "]" => 2,
+    "}" => 3,
+    ">" => 4
+  }.freeze
+
+  PARENTHESIS = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+    "<" => ">"
+  }.freeze
+
+  def initialize(input_file)
+    super input_file
+    @score = 0
+  end
+
+  def part1
+    input.each { |line| parenthesization(line) }
+    score
+  end
+
+  def part2
+    points = []
+    input.each do |line|
+      stack = parenthesization(line)
+      next if stack == :corrupted || stack.empty?
+
+      points << calculate_points(stack)
+    end
+
+    median points
+  end
+
+  private
+
+    def median(array)
+      return nil if array.empty?
+
+      sorted = array.sort
+      len = sorted.length
+      (sorted[(len - 1) / 2] + sorted[len / 2]) / 2
+    end
+
+
+    def calculate_points(stack)
+      points = 0
+      stack.reverse.each do |char|
+        points = (points * 5) + POINTS[PARENTHESIS[char]]
+      end
+
+      points
+    end
+
+    def parenthesization(line)
+      return :corrupted if PARENTHESIS.values.include? line[0]
+
+      corrupted = false
+      stack = []
+      line.split("").each do |char|
+        if PARENTHESIS.keys.include?(char)
+          stack << char
+        else
+          opening = stack.pop
+          if PARENTHESIS[opening] != char
+            @score += SCORES[char] unless corrupted
+            corrupted = true
+          end
+        end
+      end
+
+      return :corrupted if corrupted
+
+      stack
+    end
+
+    def prepare_input(input_file)
+      File.readlines(input_file).map { |l| l.strip }
+    end
+end
+
+solver = Main.new("src/day10/input.txt")
+puts "PART 1: #{solver.part1}"
+puts "PART 2: #{solver.part2}"
